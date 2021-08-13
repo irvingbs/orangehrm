d9c9d8647b70d9efffa69f96bf2d4afb64991abb
irvingbs <69214929+irvingbs@users.noreply.github.com>
2021-08-12 21:32:33 -0500

    Agregas el metodo Inicializar
    Inicializan los drivers con un Switch

--- src/test/java/base/MetodosComunes.java
+++ src/test/java/base/MetodosComunes.java
@@ -1,8 +1,51 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.testng.Reporter;
+
 public class MetodosComunes {
 
-	
+	WebDriver driver;
+	public void inicialiarExplorador(String URL, String explorador) {
+		
+		switch(explorador) {
+		case "chrome":
+			System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "\\chromedriver\\chromedriver.exe");
+			ChromeOptions option = new ChromeOptions();
+			option.addArguments("--incognito");
+			option.addArguments("--start-maximized");
+			driver = new ChromeDriver(option);
+			driver.get(URL);
+		break;
+		
+		case "msedge":
+			System.setProperty("webdriver.msedge.driver", System.getProperty("user.dir") + "\\edgedriver\\msedgedriver.exe");
+			EdgeOptions edgOption = new EdgeOptions();
+			edgOption.addArguments("--incognito");
+			edgOption.addArguments("--start-maximized");
+			driver = new EdgeDriver();
+			driver.get(URL);
+		break;
+		
+		case "firefox":
+			System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "\\geckodriver\\geckodriver.exe");
+			FirefoxOptions fOption = new FirefoxOptions();
+			fOption.addArguments("--incognito");
+			fOption.addArguments("--start-maximized");
+			driver = new FirefoxDriver();
+			driver.get(URL);
+		break;
+		default:
+			Reporter.log("El explorador que intentas abrir no existe [ "+explorador+" ]",true);
+			
+		}//End Switch
+	}
 }
 /*
  * Meeting ID: 774 3581 7005 Passcode: v5iC1P
